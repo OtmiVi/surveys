bc38f03cc047c2a5872a4a2849fcda3c6bf83c5b
OtmiVi <kyzivyra.3@gmail.com>
2023-08-06 22:57:29 +0300

    Add getters fields

--- app/models/User.php
+++ app/models/User.php
@@ -6,5 +6,33 @@ use App\Core\Model;
 
 class User extends Model
 {
-    public string $name;
+    public function __construct(
+        private int $id,
+        private string $mail,
+        private string $password
+    ){}
+
+    /**
+     * @return string
+     */
+    public function getMail(): string
+    {
+        return $this->mail;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
 }
